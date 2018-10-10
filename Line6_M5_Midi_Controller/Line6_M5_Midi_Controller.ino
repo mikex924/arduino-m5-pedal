711906d09d7c000ec016bd01566650c8f5c90582
Michael Xing <michaelx924@gmail.com>
2018-10-09 23:57:36 -0700

    added tuner button and leds

--- Line6_M5_Midi_Controller/Line6_M5_Midi_Controller.ino
+++ Line6_M5_Midi_Controller/Line6_M5_Midi_Controller.ino
@@ -3,23 +3,38 @@
 #include <JC_Button.h>
 
 MIDI_CREATE_DEFAULT_INSTANCE();
-Button buttonUp(2, 25, false, false);
-Button buttonDown(3, 25, false, false);
-Button buttonExp(4, 25, false, false);
+
+Button tunerButton(2, 25, false, false);
+Button upButton(3, 25, false, false);
+Button downButton(4, 25, false, false);
+Button expButton(5, 25, false, false);
+
+const int redPin = 8;
+const int greenPin = 9;
+const int bluePin = 10;
 
 const int presetMemAddr = 0;
 
-const int maxExp = 127;
+const int expChannel = 1;
+const int tunerChannel = 69;
+const int maxValue = 127;
 const int maxPreset = 23;
 
 int preset = 0;
+bool tunerMode = false;
 
 void setup()
 {
   MIDI.begin(1);
-  buttonUp.begin();
-  buttonDown.begin();
-  buttonExp.begin();
+
+  tunerButton.begin();
+  upButton.begin();
+  downButton.begin();
+  expButton.begin();
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
 
   int savedPreset = EEPROM.read(presetMemAddr);
   if (0 <= savedPreset && savedPreset <= maxPreset) {
@@ -30,25 +45,47 @@ void setup()
   delay(2000);
 
   changeToPreset();
+  ledNormal();
 }
 
 void loop()
 {
-  buttonUp.read();
-  buttonDown.read();
-  buttonExp.read();
+  tunerButton.read();
+  upButton.read();
+  downButton.read();
+  expButton.read();
 
-  if (buttonUp.wasPressed()) {
-    up();
-  } else if (buttonDown.wasPressed()) {
-    down();
-  } else if (buttonExp.wasPressed()) {
-    expMax();
-  } else if (buttonExp.wasReleased()) {
-    expMin();
+  if (tunerMode) {
+    if (tunerButton.wasPressed() || upButton.wasPressed() || downButton.wasPressed() || expButton.wasPressed()) {
+      tunerOff();
+    }
+  } else {
+    if (tunerButton.wasPressed()) {
+      tunerOn();
+    } else if (upButton.wasPressed()) {
+      up();
+    } else if (downButton.wasPressed()) {
+      down();
+    } else if (expButton.wasPressed()) {
+      expMax();
+    } else if (expButton.wasReleased()) {
+      expMin();
+    }
   }
 
   delay(10);
+}
+
+void tunerOn() {
+  MIDI.sendControlChange(tunerChannel, maxValue, 1);
+  tunerMode = true;
+  ledTuner();
+}
+
+void tunerOff() {
+  MIDI.sendControlChange(tunerChannel, 0, 1);
+  tunerMode = false;
+  ledNormal();
 }
 
 void up()
@@ -70,10 +107,30 @@ void changeToPreset() {
 
 void expMin()
 {
-  MIDI.sendControlChange(1, 0, 1);
+  MIDI.sendControlChange(expChannel, 0, 1);
+  ledNormal();
 }
 
 void expMax()
 {
-  MIDI.sendControlChange(1, maxExp, 1);
+  MIDI.sendControlChange(expChannel, maxValue, 1);
+  ledExp();
+}
+
+void ledNormal() {
+  analogWrite(redPin, 0);
+  analogWrite(greenPin, 50);
+  analogWrite(bluePin, 25);
+}
+
+void ledExp() {
+  analogWrite(redPin, 0);
+  analogWrite(greenPin, 70);
+  analogWrite(bluePin, 15);
+}
+
+void ledTuner() {
+  analogWrite(redPin, 150);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 25);
 }
